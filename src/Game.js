aec6f30619ec61cd8d58cc107f7b9b496f20e9ce
John Pieri <jvpieri@gmail.com>
2024-02-20 19:45:24 -0400

    Added Helis, Troopers, and Collisions

--- src/Game.js
+++ src/Game.js
@@ -39,6 +39,12 @@ class Game {
 
         if (this.frames % 600 === 0) {
             this.helicoptersArr.push(new Helicopter(this.width))
+
+        }
+
+        if (this.frames > 600 && this.frames % 120 == 0) {
+            let randomIndex = Math.floor(Math.random() * this.helicoptersArr.length)
+            this.paratroopersArr.push(this.helicoptersArr[randomIndex].dropParatrooper())
         }
 
         this.update()
@@ -53,8 +59,24 @@ class Game {
     update() {
         this.cannon.updateProjectiles()
 
+        this.cannon.projectiles.forEach((projectile) => {
+            this.helicoptersArr.forEach((helicopter) => {
+                if (this.didCollide(projectile.projectile, helicopter.helicopterEl)) {
+                    console.log("COLLIDING!!!!")
+                }
+            })
+            this.paratroopersArr.forEach((trooper) => {
+                if (this.didCollide(projectile.projectile, trooper.paratrooperEl)) {
+                    console.log("COLLIDING!!!!")
+                }
+            })
+        })
+
         this.helicoptersArr.forEach(helicopter => {
             helicopter.updateHelicopter()
+        })
+        this.paratroopersArr.forEach((troop) => {
+            troop.update()
         })
 
 
@@ -65,4 +87,22 @@ class Game {
         this.gameContainer.style.display = "none"
         this.endScreen.style.display = "flex"
     }
+
+    didCollide(projectile, obstacle) {
+        const projectileRect = projectile.getBoundingClientRect();
+        const obstacleRect = obstacle.getBoundingClientRect();
+    
+        if (
+          projectileRect.left < obstacleRect.right &&
+          projectileRect.right > obstacleRect.left &&
+          projectileRect.top < obstacleRect.bottom &&
+          projectileRect.bottom > obstacleRect.top
+        ) {
+          console.log("Colliding"); 
+          return true;
+        } else {
+          return false;
+        }
+      }
+
 }
--- src/Helicopter.js
+++ src/Helicopter.js
@@ -3,7 +3,11 @@ class Helicopter {
         this.width = 60
         this.height = 30
         this.gameWidth = gameWidth
+
+        // ! Randomly spawn the heli on either side of the screen at a random height 
         this.position = {x: Math.random() < 0.5 ? -this.width : gameWidth, y: 25 + Math.random() * 100}
+
+        // ! Dictates the direction to move based on wich side it spawned (dictated by this.position).
         this.speed = this.position.x < 0  ? 1.25 : -1.25
 
         this.helicopterEl = document.createElement("div")
@@ -18,16 +22,26 @@ class Helicopter {
 
 
     updateHelicopter() {
-        console.log(this.position.x)
+        // console.log(this.position.x)
+
+        // ! Update the helicopter's horizontal position by its speed to make it move
+
         this.position.x += this.speed
+  
+        // ! If the heli exits the screen on either side it will turn back
+
         if(this.position.x > this.gameWidth || this.position.x < -this.width) {
+            // If I multiply the speed by negative one it should reverse it's direction
             this.speed *= -1
         }
+
+        // ? Update the position of the helicopter element
+
         this.helicopterEl.style.left = `${this.position.x}px`
     }
 
     dropParatrooper() {
-        return new Paratrooper(this.position.x, this.position.y + this.height)
+        return new Paratrooper(this.position.x + 25, this.position.y + this.height)
     }
 
     removeHelicopter() {
