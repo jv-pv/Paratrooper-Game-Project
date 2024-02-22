226937d0019d6e6b328b78f68fdca539416ed188
John Pieri <jvpieri@gmail.com>
2024-02-22 11:59:31 -0400

    Adjusted heli speed to make them move faster.

--- src/Helicopter.js
+++ src/Helicopter.js
@@ -8,7 +8,7 @@ class Helicopter {
         this.position = {x: Math.random() < 0.5 ? -this.width : gameWidth, y: 25 + Math.random() * 100}
 
         // ! Dictates the direction to move based on wich side it spawned (dictated by this.position).
-        this.speed = this.position.x < 0  ? 0.75 : -0.75
+        this.speed = this.position.x < 0  ? 0.90 : -0.90
 
         this.helicopterImg = document.createElement("img")
 
